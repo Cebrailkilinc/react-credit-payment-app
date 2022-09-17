038a17b0fbd57826c14c1c6f49f10ddd2b1b3000
cebrail <cebrailyyy57@outlook.com>
2022-09-17 14:54:14 +0300

    update installment

--- src/Components/Installment .js
+++ src/Components/Installment .js
@@ -10,8 +10,8 @@ function Installment() {
     return (
         <div className='mt-10 sm:mt-16 flex flex-col  '>
             <h1 className='font-semibold'>Vade</h1>
-            <input type="number" onChange={(e)=>{setVadeSayisi(e.target.value)}}  placeholder='Vade Sayısı' className='border h-12 p-2 w-28 sm:w-56  my-2 sm:my-5 focus:outline-none ' />
-            <input type="range" className='w-80 sm:w-96  h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer  ' />
+            <input type="number" value={vadeSayisi} onChange={(e)=>{setVadeSayisi(e.target.value)}}  placeholder='Vade Sayısı' className='border h-12 p-2 w-28 sm:w-56  my-2 sm:my-5 focus:outline-none ' />
+            <input type="range" min="0" max="48" onChange={(e)=>{setVadeSayisi(e.target.value)}} value={vadeSayisi} className='w-80 sm:w-96  h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer  ' />
         </div>
     )
 }
